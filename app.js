0d3549376bf10a9990f150971ef3bcbb8c8b9edb
Dicky Adhi Satria <dicky2ux@gmail.com>
2023-02-24 21:42:38 +0700

    second commit

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
 const indexRouter = require('./routes/index');
 const uploadRouter = require('./routes/s3.routes');
+const cors = require('cors');
+
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/api', uploadRouter);
 
@@ -40,4 +42,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
+
+// module.exports = app;
